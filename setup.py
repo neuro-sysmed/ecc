97e392b7a891d7978177488e16339e11f41566d6
brugger <kim.brugger@uib.no>
2021-02-18 14:15:22 +0100

    updated setup.py for ecc

--- setup.py
+++ setup.py
@@ -11,42 +11,35 @@ def get_version():
 
     return "{}.{}.{}".format( data['major'], data['minor'], data['patch'])
 
+def get_requirements():
+
+    file_handle = open('requirements.txt', 'r')
+    data = file_handle.read()
+    file_handle.close()
+
+
+    print( data )
+    return data.split("\n")
+
+
+def scripts(directory='bin/*') -> []:
+    return glob.glob( directory )
+
 
 
 setup(name='ehos',
       version= get_version(),
-      description='Elastic HTCondor OpenStack Scaling',
-      url='https://github.com/elixir-no-nels/ehos-python/',
+      description='Elastic Compute Cluster',
+      url='https://github.com/usegalaxy-no/ecc/',
       author='Kim Brugger',
       author_email='kim.brugger@uib.no',
       license='MIT',
-      packages=['ehos'],
-      install_requires=[
-          'htcondor',
-          'typing',
-          'munch',
-          'openstacksdk',
-          'records',
-      ],
+      packages=['ecc'],
       classifiers=[
         'License :: MIT License',
         'Programming Language :: Python :: +3.6'
-        ],      
-      scripts=['bin/ehosd.py',
-               'bin/ehos_deployment.py',
-               'bin/ehos_create_config.py',
-               'bin/ehos_cloud_setup.py',
-               'bin/condor_run_jobs.py',
-               'bin/ehosd_config.py',
-               'bin/ehos_status.py'
-           ],
-      # install our config files into an ehos share.
-      data_files=[('share/ehos/', ['share/base.yaml',
-                                   'share/master.yaml',
-                                   'share/execute.yaml',]),
-                  ('etc/ehos/', ['etc/ehos.yaml.example',
-                                 'etc/ehos.yaml.template']),
- #                 ('/etc/systemd/system/',['ehos.service'])
-                 ],
+        ],
+      install_requires=[ get_requirements() ],
+      scripts=[ scripts() ],
       include_package_data=True,
       zip_safe=False)

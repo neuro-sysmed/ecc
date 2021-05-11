fb14be9cd47b5895d73e8301afaf4fcbbb6f7fe8
brugger <kim.brugger@uib.no>
2021-05-11 10:56:52 +0200

    bug fixes

--- ecc/azure_class.py
+++ ecc/azure_class.py
@@ -55,7 +55,7 @@ class Azure(object):
 
       interface_name = f"{name}-eth0"
 
-      network_interface = network_client.network_interfaces.begin_create_or_update(
+      network_interface = self._network_client.network_interfaces.begin_create_or_update(
                      network_group_name,
                      interface_name,
                      { 'location': "westeurope",
@@ -71,7 +71,7 @@ class Azure(object):
 
       print( network_interface )
 
-      vm = compute_client.virtual_machines.begin_create_or_update(
+      vm = self._compute_client.virtual_machines.begin_create_or_update(
                 compute_group_name,
                 name,
                 { "location": "westeurope",
